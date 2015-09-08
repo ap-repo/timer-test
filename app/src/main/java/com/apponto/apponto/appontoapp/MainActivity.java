f4c1aeb6b2c10a4173cfcd8a4d3e921198420d5d
José Luiz Albernaz Gomes Junior <junior.gomes@unear.net>
2015-09-08 15:19:40 -0300

    Commit Inicial
    Commit Inicial

---
+++ app/src/main/java/com/apponto/apponto/appontoapp/MainActivity.java
@@ -0,0 +1,90 @@
+package com.apponto.apponto.appontoapp;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+    private ProgressDialog progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.container);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setAppCacheEnabled(true);
+        mWebView.getSettings().setDatabaseEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
+
+        progressBar = ProgressDialog.show(this, "Apponto", "Carregando...");
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                if (progressBar.isShowing()) {
+                    progressBar.dismiss();
+                }
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        Map<String, String> headers= new HashMap<String, String>();
+        headers.put("Accept-Encoding","gzip, deflate");
+        mWebView.loadUrl("http://www.app.apponto.net/App/#/autenticacao/entrar", headers);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
